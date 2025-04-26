ab3c2517f2abe23106d32b7a1e267ce0f6c9db37
eryan <eryanwcp@gmail.com>
2025-04-26 22:29:42 +0800

    1

--- lib/src/image_file.dart
+++ lib/src/image_file.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 class ImageFile {
   final String key;
   String? fileId;
+  String? fileSrc;
   dynamic data;
   final String name;
   final String? extension;
@@ -18,7 +19,7 @@ class ImageFile {
   int get size => bytes?.length ?? 0;
 
   ImageFile(this.key,
-      {required this.name,this.fileId,this.data, this.extension, this.bytes, this.path,this.lastModified});
+      {required this.name,this.fileId,this.fileSrc,this.data, this.extension, this.bytes, this.path,this.lastModified});
 
   List<Object?> get props => [path, bytes];
 
@@ -27,6 +28,7 @@ class ImageFile {
     return '''{
       'key': $key,
       'fileId': $fileId,
+      'fileSrc': $fileSrc,
       'data': $data,
       'name': $name,
       'extension': $extension,
